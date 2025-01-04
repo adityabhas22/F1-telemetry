a5fcc7d8e0c4ea28407e0e139506c0c5fbfc9ab2
adityabhas22 <62494443+adityabhas22@users.noreply.github.com>
2025-01-04 23:42:33 +0530

    fixed gunicorn

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,3 @@
-import uvicorn
+from main import app
 
-if __name__ == "__main__":
-    uvicorn.run(
-        "app.main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True
-    ) 
+# This file is used by Gunicorn to run the FastAPI application 
